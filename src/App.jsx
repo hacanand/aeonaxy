a189761e85c0f6850764a4003ff714c237147e29
anand <anandpromax@gmail.com>
2024-04-02 01:50:59 +0530

    Add Hero component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import NavigationBar from './components/NavigationBar'
+import Hero from './components/Hero'
 
 function App() {
   return (
-    <>
+    <div className='w-full h-full  '>
       <div className="mx-8 p-2">
         <NavigationBar />
       </div>
-      <div className='  bg-zinc-200 shadow h-[1px]'></div>
-     
-    </>
+      <div className=' bg-zinc-200 shadow h-[1px]'></div>
+      <div className='mx-8 p-2  flex flex-col gap-5'>
+         <Hero/> 
+      </div>
+    
+    </div>
   );
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import img1 from "../assets/img1.jpg";
+import img2 from "../assets/img2.jpg";
+import img3 from "../assets/img3.jpg";
+function Hero() {
+  return (
+    <div className="w-full h-full p-2 mt-10">
+      <div className="flex flex-col  items-center justify-between">
+        <span className="text-5xl font-medium">Global Payment</span>
+        <span className="text-5xl font-medium"> processing solutions</span>
+        <div className="flex flex-col">
+          <span className="font-medium mt-5">
+            Grow optimise your business with a flexible,end-to-end payments
+            platform
+          </span>
+          <span className="font-medium">powered by Braintree technology</span>
+        </div>
+        <button className="bg-blue-900 mt-10  text-white px-4 p-3 rounded-full ">
+          Connect with a Payments Expert
+        </button>
+      </div>
+      <div className="bg-gray-200  mt-16">
+        <div class="mt-3 flex -space-x-2 relative ">
+          <img
+            class="inline-block w-7/12 absolute rounded-xl hover:scale-105 transition duration-500 ease-in-out"
+            src={img1}
+            alt=""
+          />
+          <img
+            class="inline-block w-7/12 rounded-xl absolute right-20 hover:scale-105 transition duration-500 ease-in-out"
+            src={img2}
+            alt=""
+          />
+        
+            <div className=" bg-white p-4 absolute right-10 rounded-xl flex flex-col  hover:scale-105 transition duration-500 ease-in-out w-6/12">
+              <img class="rounded-xl" src={img3} alt="" />
+              <div className="flex justify-between p-2 mt-4">
+                <div className="text-gray-400  font-medium text-xl">
+                  Speckled bowl set
+                  <div className="font-normal text-base">Set of two</div>
+                </div>
+                <div className="text-gray-400  font-medium text-xl">$24.00</div>
+              </div>
+              <button className=" bg-slate-300 rounded-full py-3 font-medium">
+                Checkout
+              </button>
+            </div>
+        
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
