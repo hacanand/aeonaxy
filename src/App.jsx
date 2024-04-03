7c05671621004e059ec1abf1ff21240f0c8ca141
anand <anandpromax@gmail.com>
2024-04-03 19:34:18 +0530

    Update App.jsx styling

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ import PaypalFooter from "./components/PaypalFooter";
 function App() {
   return (
     <div className="w-full h-full  ">
-      <div className="mx-8 p-2">
+      <div className="mx-4 p-2">
         <NavigationBar />
       </div>
       <div className=" bg-zinc-200 shadow h-[1px]"></div>
-      <div className="mx-10 p-2 flex flex-col gap-8">
+      <div className="mx-4 p-2 flex flex-col gap-8">
         <Hero />
         <PaypalStats />
         <PaypalPayMethods />
@@ -31,7 +31,7 @@ function App() {
       <div className="mt-20">
         <PaypalApprovalCard />
       </div>
-      <div className="mx-10 p-2 flex flex-col gap-8">
+      <div className="mx-4 p-2 flex flex-col gap-8">
         <WhyPaypal />
         <PaypalOrchestration />
         <WhyPaypal />
@@ -39,7 +39,7 @@ function App() {
       <div className="mt-20">
         <PaypalMangeRisk />
       </div>
-      <div className="mx-10 p-2 flex flex-col gap-8">
+      <div className="mx-4 p-2 flex flex-col gap-8">
         <WhyPaypal />
         <div className=" flex font-semibold text-blue-600 text-wrap">
           About Fraud Protection
@@ -57,13 +57,13 @@ function App() {
       <div className="mt-20">
         <PaypalCMO />
       </div>
-      <div className="flex mx-10 flex-col gap-8">
+      <div className="flex mx-4 flex-col gap-8">
         <PaypalIntegration />
       </div>
       <div className="mt-20">
         <PaypalContact />
       </div>
-      <div className="flex mx-10 flex-col gap-8">
+      <div className="flex mx-4 flex-col gap-8">
         <PaypalQuotes />
         <PaypalFooter/>
       </div>
